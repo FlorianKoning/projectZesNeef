c332acc8df3612da20c75d6277139d78bb88aa59
FlorianKoning <95281546+FlorianKoning@users.noreply.github.com>
2023-02-13 23:08:27 +0000

    CRUD gemaakt voor artikelen en kleine veranderingen aan dbh.php

---
+++ includes/classes/artikelen.php
@@ -0,0 +1,75 @@
+<?php
+include_once 'includes/classes/dbh.php';
+
+class Test extends Dbh {
+    
+    // reads alle omschrijvingen van de artikelen
+    public function getOmschrijving() {
+
+        $sql = "SELECT * FROM artikelen";
+        $stmst = $this->connect()->query($sql);
+        // gebruikt hier fetch omdat er toch geen user input is, dus kan er geen SQL injectie komen 
+        while ($row = $stmst->fetch()) {
+            echo $row['artOmschrijving'] . '<br>';
+        }
+    }
+
+    // reads alle omschrijvingen van de artikelen van de gegeven ID
+    public function getOmschrijvingStmt($ID) {
+
+        // hier ook gewoon fetch voor de zelfde reden
+        $sql = "SELECT * FROM artikelen WHERE artId = ?";
+        $stmst = $this->connect()->prepare($sql);
+        $stmst->execute([$ID]);
+        $ids = $stmst->fetchAll();
+
+        foreach($ids as $id) {
+            echo $id['artOmschrijving'] . '<br>';
+        }
+    }
+
+    // creates een nieuwe tabel voor artikelen
+    public function setOmschrijvingStmt($artOmschrijving, $artInkoop, $artVerkoop, $artMinVoorraad, $artMaxVoorraad, $artLocatie, $levId) {
+
+        // maakt SQL code voor de databas en gebruikt prepare zodat er geen SQL injectie kan gedaan worden
+        $sql = "INSERT INTO artikelen(artOmschrijving, artInkoop, artVerkoop, artMinVoorraad, artMaxVoorraad, artLocatie, levId) 
+        VALUES (?, ?, ?, ?, ?, ?, ?)";
+        $stmt = $this->connect()->prepare($sql);
+
+        // doet de execute en checkt of de execute is goed gegaan en console logged of het wel of niet goed is gegaan
+        $stmt->execute([$artOmschrijving, $artInkoop, $artVerkoop, $artMinVoorraad, $artMaxVoorraad, $artLocatie, $levId]);
+        if ($stmt->execute()) {
+            echo "<script>console.log('nieuw artikel toegevoegt aan de database')</script>";
+        } else {
+            echo "<script>console.log('nieuw artikel kon niet worden toegevoegt aan de database')</script>";
+        }
+    }
+
+    public function updateAllArtikelen($artOmschrijving, $artInkoop, $artVerkoop, $artMinVoorraad, $artMaxVoorraad, $artLocatie, $levId, $artId,) {
+        $sql = "UPDATE artikelen SET artOmschrijving = ?, artInkoop = ?, artVerkoop = ?, artMinVoorraad = ?, artMaxVoorraad = ?, 
+        artLocatie = ?, levId = ? WHERE artId = ?";
+        $stmt = $this->connect()->prepare($sql);
+
+        // doet de execute en kijkt of hij is goed gegaan
+        $stmt->execute([$artOmschrijving, $artInkoop, $artVerkoop, $artMinVoorraad, $artMaxVoorraad, $artLocatie, $levId, $artId]);
+        if ($stmt->execute()) {
+            echo "<script>console.log('Update van de artikel is gelukt')</script>";
+        } else {
+            echo "<script>console.log('Er is iets fout gegaan, kon niet de artikel updaten')</script>";
+        }
+    }
+
+    public function deleteArtikelen($artId) {
+        // SLQ code voor de delete
+        $sql = "DELETE FROM artikelen WHERE artId = ?";
+        $stmt = $this->connect()->prepare($sql);
+
+        // executes de sql code in de database en checkt of het goed is gegaan of niet
+        $stmt->execute([$artId]);
+        if ($stmt->execute()) {
+            echo "<script>console.log('artikel has been deleted.')</script>";
+        } else {
+            echo "<script>console.log('Er is iets fout gegaan, kon niet de artikel deleten.')</script>";
+        }
+    }
+}
--- includes/classes/dbh.php
+++ includes/classes/dbh.php
@@ -3,7 +3,6 @@
 class Dbh
 {
     // variabelen
-    private $dbConnectie;
     private $IP;
     private $DBName;
     private $userName;
@@ -20,21 +19,13 @@ class Dbh
         // maakt de connectie met de database en geeft alert box wanneer er iets fout gaat
         try {
             // verbind met de database
-            $this->dbConnectie = new PDO("mysql:host =" . $this->IP . ";dbname=" . $this->DBName, $this->userName, $this->password);
-            $this->dbConnectie->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Database Connected.<br>";
-            return $this->dbConnectie;
+            $dbConnectie = new PDO("mysql:host =" . $this->IP . ";dbname=" . $this->DBName, $this->userName, $this->password);
+            $dbConnectie->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            echo "database connected";
+            return $dbConnectie;
         } catch (PDOException $e) {
             // maakt de alert box
-            die("<script>alert('Er is iets fout gegaan met de database.')</script>" . $e->getMessage());
+            die("<script>alert('Er is iets fout gegaan met de database!')</script>" . $e->getMessage());
         }
-    }
-
-    // ben vergeten wat dit deet lol
-    public function SQLCommando($sqlCommando, $values)
-    {
-        $query = $this->dbConnectie->prepare($sqlCommando);
-        $query->execute($values);
-        return $query->fetchAll(PDO::FETCH_ASSOC);
     }
 }
